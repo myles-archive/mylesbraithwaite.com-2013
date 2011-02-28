e04abce67d7e75a190162043c11453f449d5aded
Myles Braithwaite <me@mylesbraithwaite.com>
2011-02-28 14:46:04 -0500

    Made a few small changes.

---
+++ media/js/mylesbraithwaite.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("a[rel*='external']").click(function() {
+        _gaq.push(['_trackPageview', '/outgoing/' + $(this).attr('href')]);
+    });
+    
+    $("div.illustration a").fancybox();
+})
