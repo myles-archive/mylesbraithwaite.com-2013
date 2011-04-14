b36c2c993809f0d89e28801d85ae84bf5d6d8233
Myles Braithwaite <me@mylesbraithwaite.com>
2011-04-14 13:21:00 -0400

    Fixed up the syntax of the markdown extension.

--- _plugins/kramdown_extensions.rb
+++ _plugins/kramdown_extensions.rb
@@ -5,13 +5,16 @@ module Kramdown::Converter
     def convert_a(el, indent)
       res = inner(el, indent)
       attr = el.attr.dup
+      
       if attr['href'] =~ /^mailto:/
         attr['href'] = obfuscate('mailto') << ":" << obfuscate(attr['href'].sub(/^mailto:/, ''))
         res = obfuscate(res)
       end
+      
       if attr['href'] =~ /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
         attr['rel'] = 'external'
       end
+      
       "<a#{html_attributes(attr)}>#{res}</a>"
     end
   end
