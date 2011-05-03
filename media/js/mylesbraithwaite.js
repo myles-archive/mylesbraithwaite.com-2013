a77cd1780f8edffbc12af89dabdea9b89b9dd74f
Myles Braithwaite <me@mylesbraithwaite.com>
2011-05-03 11:21:01 -0400

    Added some stupid easter eggs.

--- media/js/mylesbraithwaite.js
+++ media/js/mylesbraithwaite.js
@@ -6,10 +6,41 @@ $(document).ready(function() {
     $("div.illustration a").fancybox();
     $("div.illustrations a").fancybox();
     
+    // If the user is comeing from a Linux based OS they should get a
+    // font more suited to their OS.
     if (navigator.platform.lastIndexOf("Linux", 0) === 0) {
         var link_css = document.createElement('link');
         link_css.href = '/media/css/font-ubuntu.css';
         link_css.rel = 'stylesheet';
         $("link[rel*='stylesheet']").after(link_css);
     };
+    
+    var d = new Date();
+    
+    // Add a little heart when it's Valentines Day.
+    if (d.getMonth() == 1 && d.getDate() == 14) {
+        var img = document.createElement('img');
+        img.src = '/media/img/icons/heart.png';
+        img.alt = 'Happy Valentines Day';
+        img.title = img.alt;
+        $("header#top h1").append(img);
+    };
+    
+    // Add a Canadian flag when it's Canada Day.
+    if (d.getMonth() == 6 && d.getDate() == 1) {
+        var img = document.createElement('img');
+        img.src = '/media/img/icons/ca.png';
+        img.alt = 'Happy Canada Day';
+        img.title = img.alt;
+        $("header#top h1").append(img);
+    }
+    
+    // Add a Cake when it's my birthday.
+    if (d.getMonth() == 8 && d.getDate() == 19) {
+        var img = document.createElement('img');
+        img.src = '/media/img/icons/cake.png';
+        img.alt = 'Today is my birthday!';
+        img.title = img.alt;
+        $("header#top h1").append(img);
+    }
 })
