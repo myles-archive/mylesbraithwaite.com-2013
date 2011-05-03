4dbc6980ef7add478f154a67112b8a1a804279f3
Myles Braithwaite <me@mylesbraithwaite.com>
2011-05-03 10:29:41 -0400

    Adding a spcial style for Linux.

--- media/js/mylesbraithwaite.js
+++ media/js/mylesbraithwaite.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
     
     $("div.illustration a").fancybox();
     $("div.illustrations a").fancybox();
+    
+    if (navigator.platform.lastIndexOf("Linux", 0) === 0) {
+        var link_css = document.createElement('link');
+        link_css.href = '/media/css/font-ubuntu.css';
+        link_css.rel = 'stylesheet';
+        $("link[rel*='stylesheet']").after(link_css);
+    };
 })
